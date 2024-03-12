ff528da47e107353167aa0d9108a627e19caae14
Erik Golinelli <erik@codekraft.it>
2024-03-12 21:27:47 +0100

    extractors wip

--- src/extractors.ts
+++ src/extractors.ts
@@ -1,12 +1,10 @@
 import * as path from 'path'
 import * as fs from 'fs'
-import { type Args, type TranslationStrings } from './types'
+import { type TranslationStrings } from './types'
 import { getCommentBlock } from './utils'
 import Parser from 'tree-sitter'
 import { jsonString, parseJsonFile } from './extractors-json'
-import { parsePHPFile } from './extractors-php'
 import { extractFileData } from './extractors-text'
-import { pkgJsonHeaders } from './extractors-maps'
 import { doTree } from './tree'
 
 /**
@@ -18,7 +16,7 @@ import { doTree } from './tree'
  * @return {TranslationStrings[]} An array of translation strings.
  */
 export function yieldParsedData(
-	parsed: Record<string, any>,
+	parsed: Record<string, string>,
 	filename: string | Parser,
 	filepath: string
 ): TranslationStrings {
@@ -95,99 +93,4 @@ export async function parseFile(
 	}
 
 	return new Promise((resolve) => resolve(null))
-}
-
-/**
- * Extracts package data from the given arguments and returns a record
- * containing the specified fields from the package.json file.
- *
- * @param {Args} args - The arguments for extracting package data.
- * @param {Record<string, string>} fields - The fields to extract from the package.json file. Default is pkgJsonHeaders.
- * @return {Record<string, string>} - A record containing the extracted package data.
- */
-export function extractPackageJson(
-	args: Args,
-	fields = pkgJsonHeaders
-): Record<string, string> {
-	// TODO: package.json "files" could be used to get the file list
-	const pkgJsonMeta: Record<string, string> = {}
-	// read the package.json file
-	const packageJsonPath = args.paths.cwd
-		? path.join(args.paths.cwd, 'package.json')
-		: 'package.json'
-	/**
-	 *  check if the package.json extract the fields from the package.json file
-	 */
-	if (fs.existsSync(packageJsonPath)) {
-		const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
-		// extract the fields from the package.json file
-		for (const field of Object.keys(fields)) {
-			if (packageJson[field] !== undefined) {
-				pkgJsonMeta[field] = packageJson[field]
-			}
-		}
-	}
-	return pkgJsonMeta
-}
-
-export function extractPhpPluginData(args: Args): Record<string, string> {
-	let fileData: Record<string, string> = {}
-	const folderPhpFile = path.join(args.paths.cwd, `${args.slug}.php`)
-
-	if (fs.existsSync(folderPhpFile)) {
-		const fileContent = fs.readFileSync(folderPhpFile, 'utf8')
-		fileData = parsePHPFile(fileContent)
-
-		if ('name' in fileData) {
-			console.log('Plugin file detected.')
-			console.log(`Plugin file: ${folderPhpFile}`)
-			args.domain = 'plugin'
-
-			return fileData
-		}
-	} else {
-		console.log('Plugin file not found.')
-		console.log(`Missing Plugin filename: ${folderPhpFile}`)
-	}
-
-	return {}
-}
-
-export function extractCssThemeData(args: Args) {
-	let fileData: Record<string, string> = {}
-	const styleCssFile = path.join(args.paths.cwd, 'style.css')
-
-	if (fs.existsSync(styleCssFile)) {
-		const fileContent = fs.readFileSync(styleCssFile, 'utf8')
-		const commentBlock = getCommentBlock(fileContent)
-		fileData = extractFileData(commentBlock)
-
-		if ('Name' in fileData) {
-			console.log('Theme stylesheet detected.')
-			console.log(`Theme stylesheet: ${styleCssFile}`)
-			args.domain = 'theme'
-
-			return fileData
-		}
-	} else {
-		console.log(`Theme stylesheet not found in ${styleCssFile}`)
-	}
-	return {}
-}
-
-/**
- * Extracts main file data based on the given arguments.
- *
- * @param {Args} args - The arguments for extracting the main file data.
- * @return {Record<string, string>} The extracted main file data.
- */
-export function extractMainFileData(args: Args): Record<string, string> {
-	if (['plugin', 'block', 'generic'].includes(args.domain)) {
-		return extractPhpPluginData(args)
-	} else if (['theme', 'theme-block'].includes(args.domain)) {
-		return extractCssThemeData(args)
-	}
-
-	console.log('No main file detected.')
-	return {}
 }

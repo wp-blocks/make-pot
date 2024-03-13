50372ff8d050f2dbe2954925e71350abd4bb9910
Erik Golinelli <erik@codekraft.it>
2024-03-13 09:57:10 +0100

    updated types

--- src/maps.ts
+++ src/maps.ts
@@ -1,22 +1,25 @@
 import * as blocki18n from './assets/block-i18n.json'
 import * as themei18n from './assets/theme-i18n.json'
+import { name, version, description } from '../package.json'
+
+export const pkgJson: Record<string, unknown> = {
+	name,
+	version,
+	description,
+}
 export const themeJson = themei18n
-
-export type ThemeJson = typeof themeJson
-export type ThemeJsonKeys = keyof typeof themeJson
-
 export const blockJson = blocki18n
 
-export type BlockJson = typeof blockJson
-export type BlockJsonKeys = keyof typeof blockJson
+export const pluginJson = blocki18n
 
-export interface pkgJsonHeaders {
+export type pkgJsonHeaders = {
 	name: string
 	url: string
 	description: string
 	author: string
+	authorEmail: string
 	version: string
-	bugs: string | { url: string; email: string } // this can be an object
+	bugs: string // this can be an object | { url: string; email: string }
 	license: string
 	repository: string
 }
@@ -25,10 +28,14 @@ export const pluginHeaders = {
 	name: 'Plugin Name',
 	url: 'Plugin URI',
 	description: 'Description',
+	version: 'Version',
+	wpMinimumVersion: 'Requires at least',
+	phpMinimumVersion: 'Requires PHP',
 	author: 'Author',
 	authorUrl: 'Author URI',
-	version: 'Version',
-	license: 'License URI',
+	license: 'License',
+	licenseUri: 'License URI',
+	updateUri: 'Update URI',
 	domainPath: 'Domain Path',
 	textDomain: 'Text Domain',
 } as const

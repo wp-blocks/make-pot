0186d155ea544dd28eaec89db0aaac02daaf6c43
Erik Golinelli <erik@codekraft.it>
2024-03-13 09:53:15 +0100

    json file parser updated

--- src/extractors/json.ts
+++ src/extractors/json.ts
@@ -1,53 +1,6 @@
-import type { JsonData, TranslationStrings } from '../types'
-import { yieldParsedData } from './'
-import {
-	BlockJson,
-	blockJson,
-	pluginHeaders,
-	ThemeJson,
-	themeJson,
-} from '../maps'
-import { GetTextComment, GetTextTranslation } from 'gettext-parser'
-import { getKeyByValue } from './utils'
-
-/**
- * Finds values in a JSON object based on a given block.
- *
- * @param {T extends BlockJson} block - The block to search for values in the JSON.
- * @param {JsonData | ThemeJson} jsonData - The JSON object to search in.
- * @return {Record<string, any>} - The found values in a record.
- */
-function findValuesInJson<T extends BlockJson>(
-	block: T,
-	jsonData: JsonData | ThemeJson
-): Record<string, unknown> {
-	const result: Record<string, unknown> = {}
-
-	/**
-	 * Recursively searches for values in JSON
-	 *
-	 * @param block the block to search in
-	 * @param json the JSON object to search in
-	 */
-	// Helper function to recursively search for values in JSON
-	const searchValues = (block: T, json: JsonData) => {
-		for (const key in block) {
-			if (
-				typeof block[key] === 'object' &&
-				typeof json[key] === 'object' &&
-				key in block
-			) {
-				searchValues(block[key] as T, json[key])
-			} else if (json[key] !== undefined) {
-				result[key] = json[key]
-			}
-		}
-	}
-
-	searchValues(block, jsonData)
-
-	return result
-}
+import type { I18nSchema } from '../types'
+import { blockJson, themeJson } from '../maps'
+import { JsonSchemaExtractor } from './schema'
 
 /**
  * Parses a JSON file and returns an array of parsed data.
@@ -58,38 +11,27 @@ function findValuesInJson<T extends BlockJson>(
  * @param {number} opts.stats.index - The index of the progress bar.
  * @return {Promise<TranslationStrings>} A promise that resolves to an object containing the parsed data.
  */
-export function parseJsonFile(opts: {
+export async function parseJsonFile(opts: {
 	sourceCode: string
 	filename: 'block.json' | 'theme.json'
 	filepath: string
-}): TranslationStrings {
-	let parsed: Record<string, unknown>
-	const JsonData = JSON.parse(opts.sourceCode)
-	// parse the file based on the filename
-	parsed = findValuesInJson(
-		JsonData,
-		opts.filename === 'block.json' ? blockJson : themeJson
-	)
-
-	let parsedDefined: Record<string, string> = {}
-	for (const key in parsed) {
-		const value = parsed[key] as string
-		const header = getKeyByValue(pluginHeaders, value)
-
-		if (header) {
-			parsedDefined = {
-				...parsedDefined,
-				[header]: value,
-			}
+}): Promise<I18nSchema> {
+	const jsonTranslations = await JsonSchemaExtractor.fromString(
+		opts.sourceCode,
+		{
+			file: opts.filename,
+			schema:
+				opts.filename === 'theme.json'
+					? JsonSchemaExtractor.themeJsonSource
+					: JsonSchemaExtractor.blockJsonSource,
+			schemaFallback:
+				opts.filename === 'theme.json'
+					? JsonSchemaExtractor.themeJsonFallback
+					: JsonSchemaExtractor.blockJsonFallback,
+			addReferences: true,
 		}
-	}
-
-	// todo: yeldParsedData should be moved to extractors
-	if (parsedDefined) {
-		// extract the strings from the file and return them as an array of objects
-		return yieldParsedData(parsedDefined, opts.filename, opts.filepath)
-	}
-	return {}
+	)
+	return jsonTranslations ?? {}
 }
 
 /**
@@ -101,36 +43,10 @@ export function parseJsonFile(opts: {
  */
 export function getJsonComment(
 	key: string,
-	type?: 'block.json' | 'theme.json'
+	type?: 'block.json' | 'theme.json' | 'readme.txt'
 ): string {
 	const comments = type === 'block.json' ? blockJson : themeJson
 	return key in Object.values(comments)
 		? comments[key as keyof typeof comments]
 		: key
-}
-
-/**
- * Generates a JSON string for a given key, data, path, and optional type.
- *
- * @param {string} key - The key for the translation string.
- * @param {string} data - The data for the translation string.
- * @param {string} path - The path of the translation string.
- * @param {'block.json' | 'theme.json'} [type] - The optional type of the translation string.
- *
- * @return {TranslationStrings} The generated translation string.
- */
-export function jsonString(
-	key: string,
-	data: string,
-	path: string,
-	type?: 'block.json' | 'theme.json'
-): GetTextTranslation {
-	return {
-		msgstr: [],
-		msgid: getJsonComment(key, type),
-		msgctxt: data,
-		comments: {
-			reference: `${path}`,
-		} as GetTextComment,
-	}
 }

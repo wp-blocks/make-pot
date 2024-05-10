a115e02b8bb284a129ea86a952959acc84c853ca
Erik Golinelli <erik@codekraft.it>
2024-05-10 11:14:10 +0200

    update cli options function (for testing purposes) now it allows arguments to be passed in as an object

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,114 +1,114 @@
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
-import process from 'process'
+import process from "node:process";
+import * as yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
-import { parseCliArgs } from './parseCli'
+import { parseCliArgs } from "./parseCli.js";
 
 /**
  * Retrieves and returns the command line arguments and options.
  *
  * @return The parsed command line arguments and options.
  */
-export function getArgs() {
-	const args = yargs(hideBin(process.argv))
-		.help('h')
-		.alias('help', 'help')
-		.usage('Usage: $0 <source> [destination] [options]')
-		.positional('sourceDirectory', {
-			describe: 'Source directory',
-			type: 'string',
+export function getArgs(userArgs = {}) {
+	const args = yargs
+		.default(hideBin(process.argv))
+		.help("h")
+		.alias("help", "help")
+		.usage("Usage: $0 <source> [destination] [options]")
+		.positional("sourceDirectory", {
+			describe: "Source directory",
+			type: "string",
 		})
-		.positional('destination', {
-			describe: 'Destination directory',
-			type: 'string',
+		.positional("destination", {
+			describe: "Destination directory",
+			type: "string",
 		})
 		.options({
 			slug: {
-				describe: 'Plugin or theme slug',
-				type: 'string',
+				describe: "Plugin or theme slug",
+				type: "string",
 			},
 			domain: {
-				describe: 'Text domain to look for in the source code',
-				type: 'string',
+				describe: "Text domain to look for in the source code",
+				type: "string",
 			},
-			'skip-js': {
-				describe: 'Skip JavaScript files',
-				type: 'boolean',
+			"skip-js": {
+				describe: "Skip JavaScript files",
+				type: "boolean",
 			},
-			'skip-php': {
-				describe: 'Skip PHP files',
-				type: 'boolean',
+			"skip-php": {
+				describe: "Skip PHP files",
+				type: "boolean",
 			},
-			'skip-blade': {
-				describe: 'Skip Blade files',
-				type: 'boolean',
+			"skip-blade": {
+				describe: "Skip Blade files",
+				type: "boolean",
 			},
-			'skip-block-json': {
-				describe: 'Skip block.json files',
-				type: 'boolean',
+			"skip-block-json": {
+				describe: "Skip block.json files",
+				type: "boolean",
 			},
-			'skip-theme-json': {
-				describe: 'Skip theme.json files',
-				type: 'boolean',
+			"skip-theme-json": {
+				describe: "Skip theme.json files",
+				type: "boolean",
 			},
-			'skip-audit': {
-				describe: 'Skip auditing of strings',
-				type: 'boolean',
+			"skip-audit": {
+				describe: "Skip auditing of strings",
+				type: "boolean",
 			},
 			headers: {
-				describe: 'Headers',
-				type: 'string',
+				describe: "Headers",
+				type: "string",
 			},
-			'file-comment': {
-				describe: 'File comment',
-				type: 'string',
+			"file-comment": {
+				describe: "File comment",
+				type: "string",
 			},
-			'package-name': {
-				describe: 'Package name',
-				type: 'string',
+			"package-name": {
+				describe: "Package name",
+				type: "string",
 			},
 			location: {
-				describe: 'Include location information',
-				type: 'boolean',
+				describe: "Include location information",
+				type: "boolean",
 			},
-			'ignore-domain': {
-				describe: 'Ignore text domain',
-				type: 'boolean',
+			"ignore-domain": {
+				describe: "Ignore text domain",
+				type: "boolean",
 			},
 			mergePaths: {
-				describe: 'Merge with existing POT file(s)',
-				type: 'string',
+				describe: "Merge with existing POT file(s)",
+				type: "string",
 			},
 			subtractPaths: {
-				describe: 'Subtract strings from existing POT file(s)',
-				type: 'string',
+				describe: "Subtract strings from existing POT file(s)",
+				type: "string",
 			},
 			subtractAndMerge: {
-				describe:
-					'Subtract and merge strings from existing POT file(s)',
-				type: 'boolean',
+				describe: "Subtract and merge strings from existing POT file(s)",
+				type: "boolean",
 			},
 			include: {
-				describe: 'Include specific files',
-				type: 'string',
+				describe: "Include specific files",
+				type: "string",
 			},
 			exclude: {
-				describe: 'Exclude specific files',
-				type: 'string',
+				describe: "Exclude specific files",
+				type: "string",
 			},
 			silent: {
-				describe: 'No output to stdout',
-				type: 'boolean',
+				describe: "No output to stdout",
+				type: "boolean",
 			},
 			json: {
-				describe: 'Output the json gettext data',
-				type: 'boolean',
+				describe: "Output the json gettext data",
+				type: "boolean",
 			},
 			output: {
-				describe: 'Output the gettext data',
-				type: 'boolean',
+				describe: "Output the gettext data",
+				type: "boolean",
 			},
 		})
-		.parseSync()
-	return parseCliArgs(args)
+		.parseSync();
+	return parseCliArgs({ ...userArgs, ...args });
 }
--- src/cli/parseCli.ts
+++ src/cli/parseCli.ts
@@ -1,54 +1,46 @@
-import yargs from 'yargs'
-import { stringstring } from '../utils'
-import * as path from 'path'
-import * as process from 'process'
-import { DEFAULT_EXCLUDED_PATH } from '../const'
-import { Args, DomainType } from '../types'
-import fs, { accessSync } from 'node:fs'
+import fs, { accessSync } from "node:fs";
+import * as path from "node:path";
+import * as process from "node:process";
+import type * as yargs from "yargs";
+import { DEFAULT_EXCLUDED_PATH } from "../const.js";
+import type { Args, DomainType } from "../types.js";
+import { stringstring } from "../utils/index.js";
 
-function isThemeOrPlugin(currentPath: string = '/', slug: string) {
-	const currentWorkingDirectory = currentPath
+function isThemeOrPlugin(currentPath = "/", slug: string) {
+	const currentWorkingDirectory = currentPath;
 
 	try {
 		accessSync(
-			path.join(currentWorkingDirectory, slug + '.php'),
-			fs.constants.R_OK
-		)
-		return 'plugin'
+			path.join(currentWorkingDirectory, `${slug}.php`),
+			fs.constants.R_OK,
+		);
+		return "plugin";
 	} catch (err) {
 		// do nothing
 		console.log(
-			'the current working directory ' +
-				currentWorkingDirectory +
-				' does not contain a ' +
-				slug +
-				'.php file'
-		)
+			`the current working directory ${currentWorkingDirectory} does not contain a ${slug}.php file`,
+		);
 	}
 
 	try {
 		accessSync(
-			path.join(currentWorkingDirectory, 'style.css'),
-			fs.constants.R_OK
-		)
-		return 'theme'
+			path.join(currentWorkingDirectory, "style.css"),
+			fs.constants.R_OK,
+		);
+		return "theme";
 	} catch (err) {
 		// do nothing
 		console.log(
-			'the current working directory ' +
-				currentWorkingDirectory +
-				' does not contain a style.css file'
-		)
+			`the current working directory ${currentWorkingDirectory} does not contain a style.css file`,
+		);
 	}
 
-	if (currentWorkingDirectory.includes('wp-content' + path.sep + 'themes')) {
-		return 'theme'
-	} else if (
-		currentWorkingDirectory.includes('wp-content' + path.sep + 'plugins')
-	) {
-		return 'plugin'
+	if (currentWorkingDirectory.includes(`wp-content${path.sep}themes`)) {
+		return "theme";
+	} else if (currentWorkingDirectory.includes(`wp-content${path.sep}plugins`)) {
+		return "plugin";
 	}
-	return 'generic'
+	return "generic";
 }
 
 /**
@@ -58,22 +50,22 @@ function isThemeOrPlugin(currentPath: string = '/', slug: string) {
  * @return {object} - An object with the parsed values from the command line arguments.
  */
 export function parseCliArgs(
-	args: yargs.PositionalOptions & yargs.Options & yargs.Arguments
+	args: yargs.PositionalOptions & yargs.Options & yargs.Arguments,
 ): Args {
 	// Get the input and output paths
-	const inputPath: string = typeof args._[0] === 'string' ? args._[0] : '.'
-	const outputPath: string = typeof args._[1] === 'string' ? args._[1] : '.'
-	const currentWorkingDirectory = process.cwd()
+	const inputPath: string = typeof args._[0] === "string" ? args._[0] : ".";
+	const outputPath: string = typeof args._[1] === "string" ? args._[1] : ".";
+	const currentWorkingDirectory = process.cwd();
 	const slug =
-		args.slug && typeof args.slug === 'string'
+		args.slug && typeof args.slug === "string"
 			? args.slug
-			: path.basename(path.resolve(currentWorkingDirectory, inputPath))
-	const cwd = path.relative(currentWorkingDirectory, inputPath)
-	const out = path.relative(currentWorkingDirectory, outputPath)
+			: path.basename(path.resolve(currentWorkingDirectory, inputPath));
+	const cwd = path.relative(currentWorkingDirectory, inputPath);
+	const out = path.relative(currentWorkingDirectory, outputPath);
 
 	/** get the domain to look for (plugin, theme, etc) */
 	const domain =
-		(args?.domain as DomainType) ?? isThemeOrPlugin(path.resolve(cwd), slug)
+		(args?.domain as DomainType) ?? isThemeOrPlugin(path.resolve(cwd), slug);
 
 	const parsedArgs: Args = {
 		slug: slug,
@@ -86,9 +78,7 @@ export function parseCliArgs(
 			json: !!args.json,
 			location: !!args?.location,
 			output: !!args?.output,
-			fileComment: args.fileComment
-				? String(args.fileComment)
-				: undefined,
+			fileComment: args.fileComment ? String(args.fileComment) : undefined,
 			skip: {
 				js: !!args.skipJs,
 				php: !!args.skipPhp,
@@ -98,22 +88,17 @@ export function parseCliArgs(
 				audit: !!args.skipAudit,
 			},
 		},
-		// Headers
-		headers: {
-			fileComment: (args.fileComment as string) ?? '',
-		},
 		// Patterns
 		patterns: {
 			mergePaths: stringstring(args.mergePaths as string) ?? [],
 			subtractPaths: stringstring(args.subtractPaths as string) ?? [],
 			subtractAndMerge: !!args.subtractAndMerge,
-			include: stringstring(args.include as string) ?? ['**'],
-			exclude:
-				stringstring(args.exclude as string) ?? DEFAULT_EXCLUDED_PATH,
+			include: stringstring(args.include as string) ?? ["**"],
+			exclude: stringstring(args.exclude as string) ?? DEFAULT_EXCLUDED_PATH,
 		},
-	}
+	};
 
-	parsedArgs.paths.root = args.root ? String(args.root) : undefined
+	parsedArgs.paths.root = args.root ? String(args.root) : undefined;
 
-	return parsedArgs
+	return parsedArgs;
 }

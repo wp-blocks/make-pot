678ac5c9857770cfd90c4ad0dbea657f7bc4846a
Erik Golinelli <erik@codekraft.it>
2025-05-12 22:36:16 +0200

    main execution cleanup

--- src/parser/exec.ts
+++ src/parser/exec.ts
@@ -1,14 +1,21 @@
-import { cpus, totalmem } from "node:os";
+import path from "node:path";
 import type { SingleBar } from "cli-progress";
 import { type GetTextTranslations, po } from "gettext-parser";
+import Tannin, { type TanninDomainMetadata } from "tannin";
 import { generateHeader, translationsHeaders } from "../extractors/headers.js";
-import { getCharset } from "../fs/fs";
-import type { Args } from "../types.js";
-import { getCopyright } from "../utils/common.js";
+import { getCharset, getEncodingCharset } from "../fs/fs";
+import type { Args, Patterns } from "../types.js";
+import { getCopyright, printStats } from "../utils/common.js";
 import { getPatterns } from "./patterns.js";
 import { processFiles } from "./process.js";
 import { initProgress } from "./progress.js";
 import { taskRunner } from "./taskRunner.js";
+
+function outputPathRecap(cwd: string, patterns: Patterns) {
+	console.log(
+		`\nScript Path: ${cwd}\nfor ${patterns.include.join()}\nignoring patterns: ${patterns.exclude.join()}\n`,
+	);
+}
 
 /**
  * Runs the parser and generates the pot file or the json file based on the command line arguments
@@ -24,21 +31,19 @@ export async function exec(args: Args): Promise<string> {
 
 	// audit
 	if (args.options?.skip.audit) {
-		console.log("Audit strings...");
+		console.log("\nAudit strings...");
 		console.log("TODO");
-		/** TODO audit strings */
+		/**
+		 * TODO audit strings
+		 *
+		 * Skips string audit where it tries to find possible mistakes in translatable strings. Useful when running in an automated environment.
+		 *
+		 **/
 		console.log("✅ Done");
 	}
 
 	/** The pot file header contains the data about the plugin or theme */
-	const potHeader = generateHeader(args);
-
-	const copyrightComment =
-		args.options?.fileComment ||
-		getCopyright(
-			args.slug,
-			(args.headers?.license as string) ?? "GPL v2 or later",
-		);
+	const potHeader = await generateHeader(args);
 
 	/** We need to find the main file data so that the definitions are extracted from the plugin or theme files */
 	let translationsUnion = translationsHeaders(args);
@@ -47,6 +52,8 @@ export async function exec(args: Args): Promise<string> {
 	 * Extract the strings from the files
 	 */
 	const patterns = getPatterns(args);
+	if (!args.options?.silent)
+		outputPathRecap(path.resolve(args.paths.cwd), patterns);
 
 	/**
 	 * The progress bar that is used to show the progress of the extraction process.
@@ -62,30 +69,39 @@ export async function exec(args: Args): Promise<string> {
 		progressBar,
 	);
 
-	if (progressBar) {
-		progressBar.stop();
-	}
-
 	if (args.options?.json) {
 		// generate the json file
-		// TODO: this should compile to Jed-formatted JSON instead of JSON5
-		return JSON.stringify([potHeader, translationsUnion.toJson()], null, 4);
-	}
+		const jedData: {
+			[p: string]: { [p: string]: TanninDomainMetadata | [string, string] };
+		} = {
+			[args.slug]: {
+				"": potHeader as TanninDomainMetadata,
+				...(translationsUnion.toJson() as { [p: string]: [string, string] }),
+			},
+		};
+		const i18n = new Tannin(jedData);
 
-	const charset = getCharset(args.options?.charset);
+		return i18n.toString();
+	}
 
 	// generate the pot file json
 	const getTextTranslations: GetTextTranslations = {
-		charset: charset === "latin1" ? "iso-8859-1" : charset,
-		headers: potHeader,
+		charset: getEncodingCharset(args.options?.charset),
+		headers: potHeader as { [headerName: string]: string },
 		translations: translationsUnion.toJson(),
 	};
 
-	// And then compile the pot file
+	// And then compile the pot file to a string
 	const pluginTranslations = po
 		.compile(getTextTranslations)
-		.toString(charset as BufferEncoding);
+		.toString(getCharset(args.options?.charset));
 
-	// return the pot file as a string with the header
+	// return the pot file as a string, prefixed with the header
+	const copyrightComment =
+		args.options?.fileComment ||
+		getCopyright(
+			args.slug,
+			(args.headers?.license as string) ?? "GPL v2 or later",
+		);
 	return `${copyrightComment}\n${pluginTranslations}`;
 }

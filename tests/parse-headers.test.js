6ef0c6566a47b7be3edee8085ef3fcd7494afe90
Erik Golinelli <erik@codekraft.it>
2025-05-17 14:28:32 +0200

    updated tests (now with the correct data)

--- tests/parse-headers.test.js
+++ tests/parse-headers.test.js
@@ -2,10 +2,11 @@ const fs = require("node:fs");
 const { describe, it } = require("node:test");
 const assert = require("node:assert");
 const { generateHeader, getAuthorFromPackage } = require("../lib");
+const { join } = require("node:path");
 
 describe("Header generation", () => {
 	describe("Should generate a valid header", () => {
-		it("from user data", async () => {
+		it("from user full data", async () => {
 			const expected = {};
 			const author = await getAuthorFromPackage({
 				authors: ["John Doe <1dHsK@example.com> (http://example.com)"],
@@ -18,21 +19,34 @@ describe("Header generation", () => {
 			});
 		});
 
+		it("from user data", async () => {
+			const expected = {};
+			const author = await getAuthorFromPackage({
+				authors: ["erik"],
+			});
+
+			assert.deepStrictEqual(author, {
+				name: "erik",
+				email: undefined,
+				website: undefined,
+			});
+		});
+
 		it("from package.json data", async () => {
 			const expected = {
 				"Project-Id-Version": "plugin 1.0.0",
-				"Report-Msgid-Bugs-To": "John Doe <erik@codekraft.it>",
+				"Report-Msgid-Bugs-To": "John Doe <bbb@ccc.ddd>",
 				"MIME-Version": "1.0",
 				"Content-Transfer-Encoding": "8bit",
 				"content-type": "text/plain; charset=iso-8859-1",
 				"plural-forms": "nplurals=2; plural=(n!=1);",
 				"POT-Creation-Date": undefined,
 				"PO-Revision-Date": undefined,
-				"Last-Translator": "John Doe <erik@codekraft.it>",
-				"Language-Team": "John Doe <erik@codekraft.it>",
+				"Last-Translator": "John Doe <bbb@ccc.ddd>",
+				"Language-Team": "John Doe <bbb@ccc.ddd>",
 				"X-Generator": "@wp-blocks/make-pot 1.5.1",
 				Language: "en",
-				"x-Domain": "plugin",
+				"X-Domain": "plugin",
 			};
 
 			const result = await generateHeader({
@@ -46,7 +60,7 @@ describe("Header generation", () => {
 					domain: "my-plugin-text-domain",
 				},
 				paths: {
-					cwd: "./tests/fixtures/plugin",
+					cwd: join(process.cwd(), "tests/fixtures/plugin"),
 				},
 			});
 
